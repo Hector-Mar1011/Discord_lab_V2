a3aa8bfd84fc25ca2b1542ddc0a62139773d855b
Héctor <hectorgarcia1893@hotmail.com>
2026-05-11 07:52:41 -0600

    gestor de comandos con nuevas variables definidas

--- discord/practica02/gestor_comandos.py
+++ discord/practica02/gestor_comandos.py
@@ -32,7 +32,55 @@ def analizar_comando(entrada_usuario):
                     "!hora - Muestra la hora actual del servidor.\n"
                     "!ayuda - Muestra esta ayuda.")
 
+        else:
+            return f" El comando '{comando}' no existe. Usa '!ayuda' para ver los comandos disponibles."
+        
+    return " Recuerda usar el prefijo '!' para darme órdenes, o pregunta algo directamente."
+
+
+
 def buscar_en_diccionario(termino):
+    if not termino:
+        return "Debes escribir qué término quieres definir. Ejemplo: !definir list"
+
+    # Base de datos simplificada (puedes reutilizar la de la practica anterior)
+    conocimiento = {
+
+        "variable": "Un espacio en memoria para almacener datos",
+        "Lista": "Colección mutable de elementos",
+        "Tupla": "Colección immutable de elementos (no se pueden cambiar)."
+
+    }
+
+    return conocimiento.get(termino, f"No encontré '{termino}' en mi base de datos.")
+
 
 
 def validar_variable(nombre):
+    """
+    Lógica pedagogica: Enseña a los alumnos las reglas de nombres en python.
+    """
+    if not nombre:
+        return "Debes escribir un nombre de variable para validar. Ejemplo: !validar mi_variable"
+
+
+    #Reglas básicas de python.
+    if nombre[0].isdigit():
+        return f" '{nombre}' no es válido: ¡!No puede comenzar con un número!"
+    if " " in nombre:
+        return f" '{nombre}' no es válido: ¡!No puede contener espacios!"
+    if not nombre.isidentifier():
+        return f" '{nombre}' contiene caracteres no permitidos (solo letras ,números y _)"
+    
+    return f" '{nombre}' es un nombre de variable válido en python."
+
+if __name__ == "__main__":
+    print("--- Agente de Lógica: Fase de Comandos ---")
+    print("Prueba comandos como: !validar 123hola o !definir lista\n")
+ 
+    while True:
+        user_input = input("Alumno >> ")
+        if user_input.lower() in ["salir", "exit"]: break
+ 
+        respuesta = analizar_comando(user_input)
+        print(f"Bot >> {respuesta}\n")    
